5b3fe72a121c95edf99bb5709988593114ad67b3
Samuel Berhan <berhansami10@gmail.com>
2025-10-20 19:36:20 -0400

    commit

--- vehicle_maintnace_tracker/lib/maintenance_log.dart
+++ vehicle_maintnace_tracker/lib/maintenance_log.dart
@@ -18,5 +18,109 @@ class MaintenanceLog {
 
 
 class MaintenanceLogScreen extends StatefulWidget{
-  const MaintenanceLogScreen
+  const MaintenanceLogScreen({super.key});
+  @override
+  State<MaintenanceLogScreen> createState() => _MaintenanceLogScreenState();
+}
+
+class _MaintenanceLogScreenState extends State<MaintenanceLogScreen>{
+  final List<MaintenanceLog> _logs = [];
+
+  //controllers for input field
+  final _formKey = GlobalKey<FormState>();
+  final _typeController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _constController = TextEditingController();
+  final _notesController = TextEditingController();
+
+
+  void _addLog(){
+    if(_formKey.currentState!.validate()){
+      setState((){
+        _logs.add(
+          MaintenanceLog(
+            type: _typeController.text,
+            date: _dateController.text,
+            const: double.parse(_constController.text),
+            notes: _notesController.text,
+          ),
+
+        );
+      });
+      _clearFields();
+    }
+  }
+
+
+  void _deleteLog(int index){
+    setState(() {
+      _logs.removeAt(index);
+    });
+  }
+
+  void _editLog(int index){
+    final log = _log[index];
+    _typeController.text = log.type;
+    _dateController.text = log.date;
+    _costController.text = log.cost.toString();
+    _notesController.text = log.notes;
+
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text(" Edit maintenance log"),
+        content: Form(
+          key:_formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _typeController,
+                  decoration: const InputDecoration(labelText: 'Service Type'),
+                  validator: (v) => v!.isEmpty ? 'Enter service type': null, // if the input value is empty return enter service type
+                ),
+                TextFormField(
+                  controller: _dateController,
+                  decoration: const InputDecoration(labelText: 'Date'),
+                  validator: (v) => v!.isEmpty ? 'Enter date': null,
+
+                ),
+                TextFormField(
+                  controller: _costController,
+                  decoration: const InputDecoration(labelText:'Cost'),
+                  keyboardType: TextInputType.number,
+                  validator: (v) => v!.isEmpty ? 'Enter cost': null,
+                ),
+                TextFormField(
+                  controller: _notesController,
+                  decoration: const InputDecoration(labelText: 'Notes'),
+                ),
+              ],),
+          ),        )
+      ),
+      actions:[
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Cancel"),
+        ),
+        ElevatedButton(
+          onPressed:(){
+            if(_formKey.currentState!.validate()){
+              setState(() {
+                _logs[index] = MaintenanceLog(
+                  type: _typeController.text,
+                  date: _dateController.text,
+                  cost: double.parse(_costController.text),
+                  notes: _notesController.text,
+                );
+              });
+              _clearFields();
+              Navigator.pop(context);
+            }
+          },
+          child: const Text("save Changes"),)
+      ],
+    );
+  }
 }
